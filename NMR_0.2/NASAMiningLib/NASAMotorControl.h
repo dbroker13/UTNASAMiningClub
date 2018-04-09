81811368ec1d629cc72a4029fdd00caa4a259536
dbroker13 <davidbroker2@gmail.com>
2018-04-08 21:08:38 -0400

    Add files via upload
    Initial commit for Arduino board level code and WiFi board code.

---
+++ NMR_0.2/NASAMiningLib/NASAMotorControl.h
@@ -0,0 +1,9 @@
+//This is the header file to define byte arrays for 
+//motor control and to define functions for the motor control
+
+String DriveMotor = "DriveMotor";
+String Forward = "Forward";
+String Reverse = "Reverse";
+String Left = "Left";
+String Right = "Right";
+String Stop = "Stop";
